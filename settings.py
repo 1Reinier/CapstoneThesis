feb476382f385ca16912a94a490a2657a42b8aa5
Reinier Maat <reiniermaat@msn.com>
2014-05-25 17:07:57 +0200

    added refs to settings pane

--- settings.py
+++ settings.py
@@ -1,3 +1,5 @@
+#encoding: UTF-8
+
 """Settings.py
 Sets all parameters and constants used in the simulation.
 """
@@ -6,11 +8,11 @@ Sets all parameters and constants used in the simulation.
 STEP_SIZE = 1
 
 # -- STATISTICS --
-RANDOM_SEED = 42
-PARETO_SHAPE = 0.670
-PARETO_SCALE = 2994218
-LOGNORMAL_MEAN = 11.910
-LOGNOMRAL_STDEV = 1.078
+RANDOM_SEED = 42         # default is 42, controls random number generation
+PARETO_SHAPE = 0.670     # from Goddard, 2014 [1]
+PARETO_SCALE = 2994218   # [1]
+LOGNORMAL_MEAN = 11.910  # [1]
+LOGNOMRAL_STDEV = 1.078  # [1]
 
 # -- BANK NETWORK --
 NUMBER_OF_BANKS_LOGNORMAL = 6300  # As in the 2014 paper by Goddard on the asset size distribution of the
@@ -25,6 +27,19 @@ DEFAULT_EQUITY_FRACTION = 0.075
 
 # -- DATA --
 DATA_FILE = 'path/to/data.csv'
+
+# -- CODE_SPECIFIC REFERENCES --
+# As mentioned in comments and docstrings:
+#
+# [1] J. Goddard, H. Liu, D. Mckillop, and J. O. S. Wilson, “The Size Distribution of US Banks and Credit Unions,”
+#       Int. J. Econ. Bus., vol. 21, no. 1, pp. 139–156, Jan. 2014.
+#
+# [2] A. Krause and S. Giansante, “Interbank lending and the spread of bank failures: A network model of systemic risk,”
+#        J. Econ. Behav. Organ., vol. 83, no. 3, pp. 583–608, 2012.
+#
+# [3] R. Gropp and F. Heider, “The Determinants of Bank Capital Structure,” Rev. Financ., vol. 14, no. 4, pp. 587–622,
+#        Mar. 2010.
+
 
 
 

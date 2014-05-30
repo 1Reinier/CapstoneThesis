77bdfd9be2eb1f39cd505130074b7ca2e5caabbd
Reinier Maat <reiniermaat@msn.com>
2014-05-30 18:26:33 +0200

    allocation fix

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ def main():
     random.seed(RANDOM_SEED)
     simulation = Controller(import_network=False, export_network=True, build_network=True)
     pickle.dump(simulation, open(PICKLE_PATH, 'w'))
-    #experiment = Experiment(asset_size=True, kappa_value=False, base_simulation_location = PICKLE_PATH)
+    experiment = Experiment(asset_size=True, kappa_value=False, base_simulation_location = PICKLE_PATH)
     print('Done.')
 
 if __name__ == '__main__':
